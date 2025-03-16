b57dc5dca6f8e6a179239429a96d6c71d7e5b18f
Roberto <milaneseroberto34@gmail.com>
2025-03-16 14:01:12 +0100

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
-package main.java.org.example;
+package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carica la prima scena
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interfacce/Login.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("La Mia Applicazione JavaFX");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
